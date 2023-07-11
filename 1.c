11cc389d534f1945652ef6fdccc52adb85c7ed79
i498yosh <138637293+i498yosh@users.noreply.github.com>
2023-07-11 23:59:44 +0900

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void){
   int n;
 
-  scanf("%d", &n);
-  printf("n = %d");
 
-  if( n & 1 == 0 ){
+  scanf("%d", &n);
+  printf("n = ");
+
+  if( (n & 1) == 0 ){
     printf("%d は偶数です\n", n);
+  }
+  else{
+    printf("%d は奇数です\n", n);
   }
   return 0;
 }
+
+//修正前は&より==の計算が優先されるため正しいif文でない。
+//&の計算部分を()で閉じることで正しく処理される。
